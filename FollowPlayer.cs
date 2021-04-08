1327ddf2ae39a340a119a2b6fc3cc47c94673dc1
mahletzelalem <73862802+mahletzelalem@users.noreply.github.com>
2021-04-08 11:31:25 -0400

    Add files via upload

---
+++ FollowPlayer.cs
@@ -0,0 +1,34 @@
+﻿// Written by Mahlet Asmare
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offSet;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+
+    //smooth speed always between 0 $ 1
+    public float smoothSpeed = 0.125f;
+    // lateUpdate is same as update but, "right after"
+    /*void FixedUpdate()
+    {
+        Vector3 desiredPosition = player.position + offSet;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothPosition;
+        desiredPosition.x = Mathf.Clamp(desiredPosition.x, minPosition.x, maxPosition.x);
+        desiredPosition.y = Mathf.Clamp(desiredPosition.y, minPosition.y, maxPosition.y);
+
+    }*/
+    void LateUpdate()
+    {
+        if (transform.position!= player.position)
+        {
+            Vector3 targetPosition = new Vector3(player.position.x, player.position.y, transform.position.z);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
+        }
+    }
+}
---
+++ LifeMagic.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LifeMagic : MonoBehaviour
+{
+    public Image lifeBar;
+    public Image manaBar;
+    public Text lifeText;
+    public Text manaText;
+
+    public float myLife;
+    public float myMana;
+
+    private float currentLife;
+    private float currentMana;
+    private float calculateLife;
+
+    void Start()
+    {
+        currentLife = myLife;
+        currentMana = myMana;
+    }
+
+    void Update()
+    {
+        if (currentLife >= 0)
+        {
+            calculateLife = currentLife / myLife;
+            lifeBar.fillAmount = Mathf.MoveTowards(lifeBar.fillAmount, calculateLife, Time.deltaTime);
+            lifeText.text = "" + (int)currentLife;
+        }
+        else
+        {
+            //Game Over Scene
+        }
+        
+
+        if (currentMana < myMana)
+        {
+            manaBar.fillAmount = Mathf.MoveTowards(manaBar.fillAmount, 1f, Time.deltaTime * 0.1f);
+            currentMana = Mathf.MoveTowards(currentMana / myMana, 1f, Time.deltaTime * 0.1f) * myMana;
+        }
+        if (currentMana < 0)
+        {
+            currentMana = 0;
+        }
+
+        manaText.text = "" + Mathf.FloorToInt(currentMana);
+
+        
+    }
+
+    public void Damage(float damage)
+    {
+        currentLife -= damage;
+    }
+    public void ReduceMana(float mana)
+    {
+        if (mana <= currentMana)
+        {
+            currentMana -= mana;
+            manaBar.fillAmount -= mana / myMana;
+        }
+        else
+        {
+            //Not enough mana!
+        }
+        
+    }
+}
